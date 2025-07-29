fbca0ea6c89dc5e9efc7c0f337223c4bc7cc91d8
RMcflinn <rosebud3g@gmail.com>
2025-07-30 11:57:55 +1200

    no,2
    spiderman like thing. Playing around with curves/ quadraticVertex

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,93 +1,159 @@
-//your parameter variables go here!
-let mainEllipseWidth = 20;
-let mainEllipseHeight = 50;
-let smallEllipseWidth= 10;
+let OutsideEllipseWidth = 40;
+let OutsideEllipseHeight = 70;
+let insideFlowerWidth= 35;
+
+
 
 
 function setup_wallpaper(pWallpaper) {
-  pWallpaper.output_mode(DEVELOP_GLYPH);
-  // pWallpaper.output_mode(GRID_WALLPAPER);
+  pWallpaper.output_mode(DEVELOP_GLYPH); // pWallpaper.output_mode(GRID_WALLPAPER);
   
   pWallpaper.resolution(FIT_TO_SCREEN);
   pWallpaper.show_guide(true); //set this to false when you're ready to print
 
-  //Grid settings
   pWallpaper.grid_settings.cell_width  = 200;
   pWallpaper.grid_settings.cell_height = 200;
-  pWallpaper.grid_settings.row_offset  = 50;
+  pWallpaper.grid_settings.row_offset  = 50; //Grid settings
 }
+
+
+
 
 function wallpaper_background() {
-  background(100); 
+  background(29, 26, 117); 
 }
 
-function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
+// The Inside Flower 
+function my_symbol() { 
   angleMode(DEGREES);
+  let angle = 360/ 8;  //how many petals/rotations of 360
+  push(); //this is where it starts looping
+  translate (100, 100);//placement
+ 
+  stroke(15);
+  strokeWeight();
+  fill(133, 70, 3, 130);
+  for (let i= 1; i < 10; i += 1) { //variable is set to i, loop continues as long as i < 10, increases by 1
+    ellipse(0, 0, 100, insideFlowerWidth);
+    rotate(angle); //goes at end 
+  }
+  pop();//finished loop
 
-  // calc the angle to rotate
-  let angle = 360 / 12;
-  //move the origin to the centre  
+
+//The Outside Flower
   push();
   translate (100, 100);
-  //style flower
-  stroke(1);
-  fill(138, 67, 21);
-  //draw flower 
-  for (let i= 0; i < 10; i += 1) {
-    ellipse(0, 0, 100, smallEllipseWidth);
+
+  smooth();
+  strokeWeight();
+  fill(200, 50);
+  for (let i= 0; i < 7; i += 1) {
+    ellipse(0, 0, OutsideEllipseHeight, OutsideEllipseWidth);
     rotate(angle);
+  }
+ pop();
+
+
+//Outside Main Lines 
+  push();
+  translate (100, 100);
+
+  stroke(255, 217, 112, 70);
+  for (let i= 0; i < 8; i +=1) {
+  line (100, 0, 0, 0);
+  rotate(angle);
   }
   pop();
 
 
-
-
-  push();
+//The Lines Outline 
+    push();
   translate (100, 100);
 
-  stroke(1);
-  fill(255, 50);
-  ellipse(0, 0, mainEllipseWidth, mainEllipseHeight);
-  rotate(angle);
-  ellipse(0, 0, mainEllipseWidth, mainEllipseHeight);
-  rotate(angle);
-  ellipse(0, 0, mainEllipseWidth, mainEllipseHeight);
-  rotate(angle);
-  ellipse(0, 0, mainEllipseWidth, mainEllipseHeight);
-  rotate(angle);
-  ellipse(0, 0, mainEllipseWidth, mainEllipseHeight);
-  rotate(angle);
-  ellipse(0, 0, mainEllipseWidth, mainEllipseHeight);
-  rotate(angle);
-  ellipse(0, 0, mainEllipseWidth, mainEllipseHeight);
-  rotate(angle);
-  // fill(255, 50);
-
-  // for (let i= 0; i < 7; i += 1) {
-  // ellipse(0, 0, 80, 50);
-  // rotate(angle);
-  // }
+  smooth();
+  strokeWeight();
+  fill(200, 50);
+  for (let i= 0; i < 7; i += 1) {
+    ellipse(0, 0, 200, 5);
+    rotate(angle);
+  }
  pop();
 
 
-  stroke(2);
-  fill(1);
+  noFill();
+  beginShape();
+
+   vertex(29, 29);
+    quadraticVertex(100, 100, 100, 0);
+    quadraticVertex(100, 100, 171, 29);
+    quadraticVertex(100, 100, 200, 100);
+    quadraticVertex(100, 100, 171, 171);
+    quadraticVertex(100, 100, 100, 200);
+    quadraticVertex(100, 100, 29, 171);
+    quadraticVertex(100, 100, 0, 100);
+    quadraticVertex(100, 100, 29, 29);
+   endShape();
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  //stroke(2);
+  //fill(1);
   //central ellipse
-  ellipse(100, 100, 5, 5);
+  //ellipse(100, 100, 5, 5);
+
+  //ellipseMode(CORNER);
+  //noFill();
+  //strokeWeight(0.25);
+  //stroke(200);
+  //ellipse(20, 20, 40, 40);
+
+  //ellipseMode(CORNER);
+  //noFill();
+  //strokeWeight(0.25);
+  //stroke(200);
+  //ellipse(145, 145, 20, 20);
 
 
 
 
-  stroke(1);
-  fill(1);
-  point()
+
+
+
+}
+  
+
+
+
+
+
+
+
+
+
+  
 
  
 
-}
-
-function dots() {
-noStroke();
 
 
-}
+
+
+
+
